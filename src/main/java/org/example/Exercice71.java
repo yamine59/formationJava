441ee21d7599d0a61f9153cac7f0955f13f6f379
Yamine59 <yamineizi22@gmail.com>
2025-05-12 16:45:54 +0200

    exercice 52 à 57

---
+++ src/main/java/org/example/Exercice71.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Exercice71 {
+    public static void main(String[] args) {
+        /*
+        1. Créer une variable caractere
+        2. Affecter une valeur à la variable caractere
+        3. A l'aide des structures conditionnelles, déterminer si le caractère
+            est une consonne ou une voyelle
+         */
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("entrer un caractère");
+        String c = scanner.nextLine();
+        c.charAt(1);
+
+
+       switch (c) {
+           case "a":
+           case "e":
+           case "i":
+           case "o":
+           case "u":
+           case "y":
+               System.out.println("c'est une voyelle");
+               break;
+           default:
+               System.out.println("c'est une consonne");
+       }
+
+
+
+    }
+}
