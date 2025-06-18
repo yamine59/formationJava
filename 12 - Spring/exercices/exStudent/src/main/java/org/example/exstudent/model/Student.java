bafbd5e21eba1113373189f3e8ed244c220e4967
Yamine59 <yamineizi22@gmail.com>
2025-06-18 15:23:21 +0200

    spring ex student

--- 12 - Spring/exercices/exStudent/src/main/java/org/example/exstudent/model/Student.java
+++ 12 - Spring/exercices/exStudent/src/main/java/org/example/exstudent/model/Student.java
@@ -11,6 +11,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+
 public class Student {
     private UUID id;
     private String firstname;
--- 12 - Spring/exercices/exStudent/src/main/java/org/example/exstudent/service/StudentService.java
+++ 12 - Spring/exercices/exStudent/src/main/java/org/example/exstudent/service/StudentService.java
@@ -9,7 +9,7 @@ import java.util.Map;
 import java.util.UUID;
 
 @Service
-public class StudentService {
+public class StudentService implements IStudentService<Student> {
 
     private final Map<UUID , Student> students;
 
@@ -18,7 +18,7 @@ public class StudentService {
     }
 
     public void saveOrUpdate(Student student) {
-        students.put(student.getId(),student);
+        students.put(student.getId(), student);
     }
 
     public Student getStudentById(UUID id){
@@ -30,8 +30,17 @@ public class StudentService {
     }
 
     public List<Student> getStudentByName (String lastname){
-        return students.values().stream().filter(s -> s.getLastname()
-                .equals(lastname)).toList();
+        if (lastname.isEmpty()){
+            return students.values().stream().toList();
+
+        } else if (lastname.length() <= 3 ) {
+
+            return students.values().stream().filter(s -> s.getLastname()
+                    .contains(lastname)).toList();
+        }else {
+            return students.values().stream().filter(s -> s.getLastname()
+                    .equals(lastname)).toList();
+        }
     }
 
     public void deleteStudent (UUID id){
