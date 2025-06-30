7a34249b6bde1c354e0cdd805efef11702aba648
Yamine59 <yamineizi22@gmail.com>
2025-06-30 13:33:17 +0200

    add: exRehercheVille

--- 13- test/exercices/tddRechercheVille/src/main/java/org/example/CitySearch.java
+++ 13- test/exercices/tddRechercheVille/src/main/java/org/example/CitySearch.java
@@ -4,23 +4,21 @@ import jdk.jshell.spi.ExecutionControl;
 import org.example.exception.NotFoundException;
 import org.example.exception.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 public class CitySearch {
-    List<String> city = List.of(
-            "paris", "budapest", "skopje",
-            "rotterdam", "valence", "vancouver",
-            "amsterdam", "vienne", "sydney",
-            "new york", "londres", "bangkok",
-            "hong kong", "duba ", "rome", "istanbul"
-    );
+    public CitySearch(List<String> city) {
+        this.city = city;
+    }
+
+    private List<String> city;
     public List<String> search(String search)  {
         if (Objects.equals(search, "*")){
             return city.stream().map(s -> s.substring(0, 1).toUpperCase() + s.substring(1)).toList();
         }
         String searchGET =  search.toLowerCase();
-        System.out.println(searchGET);
 
         if (searchGET.length()<2){
             throw new NotFoundException("City name must be at least 2 characters");
--- 13- test/exercices/tddRechercheVille/src/test/java/org/example/CitySearchTest.java
+++ 13- test/exercices/tddRechercheVille/src/test/java/org/example/CitySearchTest.java
@@ -3,40 +3,51 @@ package org.example;
 import org.example.exception.NotFoundException;
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import javax.swing.plaf.PanelUI;
 import java.util.List;
 
 public class CitySearchTest {
+    List<String> city = List.of(
+            "paris", "budapest", "skopje",
+            "rotterdam", "valence", "vancouver",
+            "amsterdam", "vienne", "sydney",
+            "new york", "londre", "bangkok",
+            "hong kong", "dubai ", "rome", "istanbul"
+    );
+
+
 
     private CitySearch citySearch;
     @Test
     public void WhenSearch_StartMin_2_ThenResult_ThrowException(){
-        citySearch = new CitySearch();
+        citySearch = new CitySearch(city);
         Assert.assertThrows(NotFoundException.class,()-> {citySearch.search("a");});
     }
     @Test
     public void WhenSearch_minOrEquals_2_ThenResults_stringCity(){
-        citySearch = new CitySearch();
+        citySearch = new CitySearch(city);
         List<String> result = citySearch.search("pa");
         Assert.assertTrue(result.stream().toList().contains("Paris"));
     }
+
     @Test
     public void WhenSearchCaseInsensitive_ThenResult_stringCity(){
-        citySearch = new CitySearch();
+        citySearch = new CitySearch(city);
         List<String> result = citySearch.search("PaR");
         System.out.println(result.stream().toList());
         Assert.assertTrue(result.stream().toList().contains("Paris"));
     }
     @Test
     public void WhenSearchContainStringOfCity_ThenResult_stringCity(){
-        citySearch = new CitySearch();
+        citySearch = new CitySearch(city);
         List<String> result = citySearch.search("RIS");
         Assert.assertTrue(result.stream().toList().contains("Paris"));
     }
     @Test
     public void WhenSearchIs_All_ThenResult_stringAllCity(){
-        citySearch = new CitySearch();
+        citySearch = new CitySearch(city);
         List<String> result = citySearch.search("*");
         Assert.assertTrue(result.size()>=10);
     }
