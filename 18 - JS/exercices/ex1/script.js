a794bbcc19da8a1ed6677c779c7071a0b3506689
Yamine59 <yamineizi22@gmail.com>
2025-07-21 16:20:35 +0200

    exercice js

--- 18 - JS/exercices/ex1/script.js
+++ 18 - JS/exercices/ex1/script.js
@@ -1,58 +1,103 @@
 const scores = [];
 scores.push(45, 100, 69, 85, 50)
 
-function afficherScores(score){
-  console.log("Scores des joueurs : ");
-  console.log(score)
+function afficherscores(scores){
+    return scores
 }
 
-function moyenneScores(score){
+function moyennescores(scores){
     let somme = 0;
-    for (let i = 0; i < score.length; i++) {
-        somme += score[i];
+    for (let i = 0; i < scores.length; i++) {
+        somme += scores[i];
     }
-    console.log("Score moyen : " + somme/score.length);
+    const moyennescores = somme/scores.length;
+    return moyennescores;
 }
 
-function meilleurScore(score){
-    let meilleur = score[0];
-    for (let i = 1; i < score.length; i++) {
-        if (score[i] > meilleur) {
-            meilleur = score[i];
+function meilleurscores(scores){
+    let meilleur = scores[0];
+    for (let i = 1; i < scores.length; i++) {
+        if (scores[i] > meilleur) {
+            meilleur = scores[i];
         }
     } 
-    console.log("Score moyen : " + meilleur);
+    return meilleur;
 }
-
-function mentionScore(score) {
+function mentionscores(scores) {
+    let result = []
     for (let i = 0 ; i < scores.length; i++) {
         let index = i+1
         switch (true) {
-            case (score[i] < 50):
-                console.log("Joueur "+ index+" : Insuffisant");
+            case (scores[i] < 50):
+                result.push("Joueur "+ index+" : Insuffisant");
                 break;
-            case (score[i] >= 50 && score[i] <= 64):
-                console.log("Joueur "+ index+" : Passable");
+            case (scores[i] >= 50 && scores[i] <= 64):
+                result.push("Joueur "+ index+" : Passable");
                 break;
-            case (score[i] >= 65 && score[i] <= 79):
-                console.log("Joueur "+ index+" : Bien");
+            case (scores[i] >= 65 && scores[i] <= 79):
+                result.push("Joueur "+ index+" : Bien");
                 break;
-            case (score[i] >= 80 && score[i] <= 89):
-                console.log("Joueur "+ index+" : Très bien");
+            case (scores[i] >= 80 && scores[i] <= 89):
+                result.push("Joueur "+ index+" : Très bien");
                 break;
-            case (score[i] >= 90 && score[i] <= 100):
-                console.log("Joueur "+ index+" : Excellent");
+            case (scores[i] >= 90 && scores[i] <= 100):
+                result.push("Joueur "+ index+" : Excellent");
                 break;
+            
            
         }
         
     }
+    return result
     
 }
 
+console.log("scores des joueurs : ",afficherscores(scores));
+console.log("scores moyen : " , moyennescores(scores));
+console.log("Meilleur scores : " , meilleurscores(scores));
+console.log("mentions des joueurs : ",mentionscores(scores));
 
 
-afficherScores(scores)
-moyenneScores(scores)
-meilleurScore(scores)
-mentionScore(scores)
+
+//with console.table
+// console.log("scores des joueurs : ");
+// console.table(afficherscores(scores));
+// console.log("scores moyen : " , moyennescores(scores));
+// console.log("Meilleur scores : " , meilleurscores(scores));
+// console.table(mentionscores(scores));
+
+function estExcellent(score){
+    if (score>= 90) {
+        return true
+    }
+    return false
+}
+function estMoyen(score){
+    if (score >= 50 && score <= 80) {
+        return true
+    }
+    return false
+}
+function estInsuffisant(score){
+    if (score < 50) {
+        return true
+    }
+    return false
+}
+
+function filtersScores(scores,callback){
+    let result = []
+    for (let i = 0; i < scores.length; i++) {
+         result.push(scores[i]+" est " + callback(scores[i]) ) 
+    }
+    return result
+    
+}
+console.log(scores);
+console.log("Filtres des scores")
+console.log("les scores excellents : ");
+console.table(filtersScores(scores,estExcellent));
+console.log("les scores moyens : ");
+console.table(filtersScores(scores,estMoyen));
+console.log("les scores insuffisants : ");
+console.table(filtersScores(scores,estInsuffisant));
